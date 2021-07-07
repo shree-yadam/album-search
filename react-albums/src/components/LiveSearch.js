aeb59e0f609540aacb67371c05b99248024af507
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-07 17:51:10 -0400

    add useEffect

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,27 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
+const axios = require("axios");
+
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`)
+      .then(function (response) {
+        // handle success
+        console.log(response.data.results);
+        setResults([...response.data.results]);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, [term]);
 
   return (
     <Fragment>
@@ -13,7 +29,7 @@ export default function LiveSearch(props) {
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
